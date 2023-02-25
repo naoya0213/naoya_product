0fae7625a717ac926197b06821becb1ed743c705
naoya0213 <124577478+naoya0213@users.noreply.github.com>
2023-02-25 15:17:18 +0900

    Update composition.py

--- composition.py
+++ composition.py
@@ -6,10 +6,7 @@ import matplotlib.pyplot as plt
 
 def background_composition(image_path, matte_path, bg_image_path):
   foreground = Image.open(image_path)
-  matte = Image.open(matte_path)
-  background = Image.open(bg_image_path).resize(foreground.size)
-  
-  composition_img = Image.composite(foreground, background, matte)
+  composition_img = Image.composite(foreground, bg_image_path, matte_path)
  
   return composition_img
 # 
