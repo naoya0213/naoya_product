29bcaaac4bc90fd4ab82ea5c04b80fb45efbcaca
naoya0213 <124577478+naoya0213@users.noreply.github.com>
2023-02-25 16:40:38 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from werkzeug.utils import secure_filename
 
 
 UPLOAD_FOLDER='./static/modnet_image/input'
-portrait_path='./static\modnet_image\output/portrait.jpg'
+portrait_path='./tmp/input.jpg'
 app = Flask(__name__)
 
 @app.route('/')
@@ -22,29 +22,24 @@ def product01():
 @app.route('/upload', methods=['GET', 'POST'])
 def upload_user_files():
     if request.method == 'POST':
-        upload_file = request.files['upload_file']
+        # upload_file = request.files['upload_file']
         # img_path = 'static\modnet_image\input\\22861417_s.jpg'# os.path.join(UPLOAD_FOLDER,upload_file.filename)
         # upload_file.save(img_path)
         # output=portrait(img_path)
         # output.save(portrait_path)
         # print(portrait_path)
         
-        f = secure_filename(upload_file.filename)
+        f = request.files['upload_file']
+        
+        filename = secure_filename(f.filename)
         path_2_tmp = os.path.join(os.path.dirname(os.path.abspath(__file__)), "tmp")
-        print(os.path.join(path_2_tmp, f))
+        # print(os.path.join(path_2_tmp, filename))
         if not os.path.exists(path_2_tmp):
             os.mkdir(path_2_tmp)
-        f.save(os.path.join(path_2_tmp, f))
-        return f
-    return render_template('portrait_result.html',portrait_path=os.path.join(path_2_tmp, f))
+        f.save(os.path.join(path_2_tmp,"input.jpg"))
+        return render_template('portrait_result.html' , portrait_path =portrait_path)
 
-@app.route('/image-upload', methods=['GET', 'POST'])
-def image_post():
-    if request.method == 'POST':
-        f = request.files['file']
-        
-        return "file req error"
-    return "post error"
+
 
 # @app.route('/fetch-image', methods=['GET', 'POST'])
 # def fetch_image():
